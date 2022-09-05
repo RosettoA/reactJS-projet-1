24b0a3075f6766acc0ad6bdc409a95a99b2e94c4
Antony <antony.rosetto@hotmail.fr>
2022-09-05 11:51:08 +0200

    Initialisation d'une application vitrine de drapeaux mondieux et divers infos associées

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium,
+        dolores eligendi alias cupiditate enim repellat impedit dolor
+        reprehenderit quos illo doloremque tempora. In numquam, expedita
+        consequatur non excepturi saepe facilis?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta deserunt
+        corrupti minima quas iste commodi hic dolor architecto! Nesciunt tenetur
+        voluptates nihil, magnam quis repellendus inventore distinctio atque
+        vitae sunt?
+      </p>
+    </div>
+  );
+};
+
+export default About;
